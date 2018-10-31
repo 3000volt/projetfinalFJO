64339aced0168d25f87b9bc73560fc1213196ded
Felix <info1@1063-R4PC4>
2018-10-31 11:32:48 -0400

    Accueil du l admin

--- projetfinalFJO/Controllers/HomeController.cs
+++ projetfinalFJO/Controllers/HomeController.cs
@@ -4,22 +4,54 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using projetfinalFJO.Appdata;
 using projetfinalFJO.Models;
 
 namespace projetfinalFJO.Controllers
 {
     public class HomeController : Controller
     {
+        //Propriétés
+        private IConfiguration config;
+        private ActualisationContext contexte;
+        List<ActualisationInformation> listeActualisation;
+        List<Programmes> listeProgrammes;
+
+        //Constructeur du controleur
+        public HomeController(IConfiguration iConfig)
+        {
+            this.config = iConfig;
+            this.contexte = new ActualisationContext();
+        }
+
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult About()
+        [HttpGet]
+        public IActionResult Actualisation()
         {
-            ViewData["Message"] = "Your application description page.";
+            listeActualisation = this.contexte.ActualisationInformation.ToList();
+            ActualisationViewModel actu = new ActualisationViewModel();
 
-            return View();
+            List<ActualisationViewModel> actuListe = listeActualisation.Select(x => new ActualisationViewModel
+            {
+                NumActualisation = x.NumActualisation,
+                NomActualisation = x.NomActualisation,
+                NoProgramme = x.NoProgramme,
+                NomProgramme = Selection(x.NoProgramme),
+                Approuve = x.Approuve
+            }).ToList();
+            return View(actuListe);
+        }
+
+        private string Selection(string num)
+        {
+            listeProgrammes = this.contexte.Programmes.ToList();
+            Programmes prog = listeProgrammes.Find(x => x.NoProgramme == num);
+            string numero = prog.NomProgramme;
+            return numero;
         }
 
         public IActionResult Contact()
