df34003b00918eed9c06caf5e4625573bdf293ab
Félix Lagacé <1471667@cegepoutaouais.qc.ca>
2018-11-07 14:04:54 -0500

    bd a jour

--- projetfinalFJO/Appdata/Competences.cs
+++ projetfinalFJO/Appdata/Competences.cs
@@ -5,28 +5,11 @@ namespace projetfinalFJO.Appdata
 {
     public partial class Competences
     {
-        public Competences()
-        {
-            AnalyseCompétence = new HashSet<AnalyseCompétence>();
-            CompetencesElementCompetence = new HashSet<CompetencesElementCompetence>();
-            CoursCompetences = new HashSet<CoursCompetences>();
-            RepartirHeureCompetence = new HashSet<RepartirHeureCompetence>();
-            RepartitionHeureCours = new HashSet<RepartitionHeureCours>();
-            RepartitionHeuresession = new HashSet<RepartitionHeuresession>();
-        }
-
         public string CodeCompetence { get; set; }
         public bool? ObligatoireCégep { get; set; }
         public string Description { get; set; }
         public string ContextRealisation { get; set; }
         public int? Idfamille { get; set; }
-
-        public Famillecompetence IdfamilleNavigation { get; set; }
-        public ICollection<AnalyseCompétence> AnalyseCompétence { get; set; }
-        public ICollection<CompetencesElementCompetence> CompetencesElementCompetence { get; set; }
-        public ICollection<CoursCompetences> CoursCompetences { get; set; }
-        public ICollection<RepartirHeureCompetence> RepartirHeureCompetence { get; set; }
-        public ICollection<RepartitionHeureCours> RepartitionHeureCours { get; set; }
-        public ICollection<RepartitionHeuresession> RepartitionHeuresession { get; set; }
+        public string NoProgramme { get; set; }
     }
 }
