d14a6cd7a6ced61065a96c17d0a6d5837be4bd28
Joel Stephane Loic Ngando-Kouo <1591982@cegepoutaouais.qc.ca>
2018-11-12 09:25:10 -0500

    joel script

--- projetfinalFJO/wwwroot/js/site.js
+++ projetfinalFJO/wwwroot/js/site.js
@@ -5,7 +5,6 @@
 // Write your JavaScript code.
 
 function fnAddcommpetenceAjax() {
-
     var url = "/Competences/Create";
     var data = {
         CodeCompetence: $("#CodeCompetence").val(),
@@ -86,63 +85,3 @@ function fnAssocierelecommpetenceAjax() {
     });
     return false;
 }
-/*
-function fndelete(roleId) {
-    $.ajax({
-        // data: jQuery.param({ id: roleId }),
-        type: "GET",
-        url: "/Role/Delete/" + roleId,
-        datatype: "text/plain",
-        contentType: "application/html; charset=utf-8",
-        success: function (result) {
-            alert(result);
-            $("#listeTable tr[id=\"" + roleId + "\"]").remove();
-        },
-        error: function (xhr, status) { alert("erreur:" + status); }
-    });
-}
-
-function fneditGet(roleId) {
-
-    $.ajax({
-        // data: jQuery.param({ id: roleId }), // envoyer le paramètre dans le querystring
-        type: "GET",
-        url: "/Role/Edit/" + roleId, // envoyer le paramètre dans l'uri
-        contentType: "application/html; charset=utf-8",
-        datatype: "json",
-        success: function (result) {
-            $("form").prop("title", "Edit");
-            formModal.dialog("open");
-            $("#Id").val(result.id);
-            $("#RoleName").val(result.roleName);
-            $("#Description").val(result.description);
-
-        },
-        error: function (xhr, status) { alert("erreur:" + status); }
-    });
-}
-function fneditPost(roleId) {
-    var data = {
-        id: $("#Id").val(),
-        RoleName: $("#RoleName").val(),
-        Description: $("#Description").val()
-    };
-    $.ajax({
-        data: JSON.stringify(data),
-        type: "POST",
-        url: "/Role/Edit/",
-        contentType: "application/json; charset=utf-8",
-        datatype: "text/plain",
-        success: function (result, textStatus) {
-            alert(textStatus + ":" + result);
-            var tr = "<td>" + $("#Id").val() + "</td><td>" + $("#RoleName").val() + "</td><td>" + $("#Description").val() + "</td>" +
-                "<td>" +
-                "<a class=\"btn btn-info\" onclick=\"fneditGet(" + $("#Id").val() + ")\"> <i class=\"glyphicon glyphicon-pencil\"></i> Edit </a>" +
-                " <a class=\"btn btn-danger\" onclick=\"fndelete(" + $("#Id").val() + ")\"><i class=\"glyphicon glyphicon-trash\"></i>Delete</a>" +
-                "</td>";
-            $("#listeTable tr[id=\"" + $("#Id").val() + "\"]").html(tr);
-
-        },
-        error: function (xhr, status) { alert("erreur:" + status); }
-    });
-}*/
