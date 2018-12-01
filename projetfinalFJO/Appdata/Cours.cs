223a3417e0eb9346b5506c206fa85093d8f57605
Olivier Goulet-Medeiros <0991038@cegepoutaouais.qc.ca>
2018-12-01 17:45:51 -0500

    Cours: update & delete marche(sans ajax), Bullshit de design encore...

--- projetfinalFJO/Appdata/Cours.cs
+++ projetfinalFJO/Appdata/Cours.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace projetfinalFJO.Appdata
 {
@@ -11,21 +12,34 @@ namespace projetfinalFJO.Appdata
             Prealables = new HashSet<Prealables>();
             RepartitionHeureCours = new HashSet<RepartitionHeureCours>();
         }
-
+        [Display(Name ="Numéro du cours")]
         public string NoCours { get; set; }
+        [Display(Name = "Nom du cours")]
         public string NomCours { get; set; }
+        [Display(Name = "Pondération")]
         public string PonderationCours { get; set; }
+        [Display(Name = "Département")]
         public string DepartementCours { get; set; }
+        [Display(Name = "Type de cours")]
         public string TypedeCours { get; set; }
+        [Display(Name = "Numéro de programme")]
         public string NoProgramme { get; set; }
+        [Display(Name = "Session")]
         public string NomSession { get; set; }
+        [Display(Name = "Groupe de compétence")]
         public string NomGroupe { get; set; }
 
+        [Display(Name = "Numéro de programme")]
         public Programmes NoProgrammeNavigation { get; set; }
+        [Display(Name = "Groupe de compétence")]
         public Groupe NomGroupeNavigation { get; set; }
+        [Display(Name = "Session")]
         public Session NomSessionNavigation { get; set; }
+        [Display(Name = "")]
         public ICollection<CoursCompetences> CoursCompetences { get; set; }
+        [Display(Name = "")]
         public ICollection<Prealables> Prealables { get; set; }
+        [Display(Name = "")]
         public ICollection<RepartitionHeureCours> RepartitionHeureCours { get; set; }
     }
 }
