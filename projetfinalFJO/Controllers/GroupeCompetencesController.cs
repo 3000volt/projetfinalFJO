4ef87a01ba0ab5b73d7d2d3ae81efb44edc765ec
Joel Stephane Loic Ngando-Kouo <1591982@cegepoutaouais.qc.ca>
2018-12-18 18:46:11 -0500

    try catch gerer utili+groupe compétence

--- projetfinalFJO/Controllers/GroupeCompetencesController.cs
+++ projetfinalFJO/Controllers/GroupeCompetencesController.cs
@@ -24,36 +24,60 @@ namespace projetfinalFJO.Controllers
         // GET: GroupeCompetences
         public async Task<IActionResult> Index()
         {
-            var actualisationContext = _context.GroupeCompetence.Include(g => g.CodeCompetenceNavigation).Include(g => g.NomGroupeNavigation).Where(x => x.NoProgramme.Equals(this.HttpContext.Session.GetString("programme")));
-            return View(await actualisationContext.ToListAsync());
+            try
+            {
+                var actualisationContext = _context.GroupeCompetence.Include(g => g.CodeCompetenceNavigation).Include(g => g.NomGroupeNavigation).Where(x => x.NoProgramme.Equals(this.HttpContext.Session.GetString("programme")));
+                return View(await actualisationContext.ToListAsync());
+            }
+            catch (Exception e)
+            {
+                return View("\\Views\\Shared\\page_erreur.cshtml");
+            }
+           
         }
 
         // GET: GroupeCompetences/Details/5
         public async Task<IActionResult> Details(string id)
         {
-            if (id == null)
+            try
             {
-                return NotFound();
-            }
+                if (id == null)
+                {
+                    return NotFound();
+                }
 
-            var groupeCompetence = await _context.GroupeCompetence
-                .Include(g => g.CodeCompetenceNavigation)
-                .Include(g => g.NomGroupeNavigation)
-                .FirstOrDefaultAsync(m => m.NomGroupe == id);
-            if (groupeCompetence == null)
+                var groupeCompetence = await _context.GroupeCompetence
+                    .Include(g => g.CodeCompetenceNavigation)
+                    .Include(g => g.NomGroupeNavigation)
+                    .FirstOrDefaultAsync(m => m.NomGroupe == id);
+                if (groupeCompetence == null)
+                {
+                    return NotFound();
+                }
+
+                return View(groupeCompetence);
+            }
+            catch (Exception e)
             {
-                return NotFound();
+                return View("\\Views\\Shared\\page_erreur.cshtml");
             }
-
-            return View(groupeCompetence);
+            
         }
 
         // GET: GroupeCompetences/Create
         public IActionResult Create()
         {
-            ViewData["CodeCompetence"] = new SelectList(_context.Competences, "CodeCompetence", "CodeCompetence");
-            ViewData["NomGroupe"] = new SelectList(_context.Groupe, "NomGroupe", "NomGroupe");
-            return View();
+            try
+            {
+                ViewData["CodeCompetence"] = new SelectList(_context.Competences, "CodeCompetence", "CodeCompetence");
+                ViewData["NomGroupe"] = new SelectList(_context.Groupe, "NomGroupe", "NomGroupe");
+                return View();
+            }
+            catch (Exception e)
+            {
+                return View("\\Views\\Shared\\page_erreur.cshtml");
+            }
+            
         }
 
         // POST: GroupeCompetences/Create
@@ -63,33 +87,49 @@ namespace projetfinalFJO.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([FromBody][Bind("NomGroupe,CodeCompetence,NomSession,NoProgramme")] GroupeCompetence groupeCompetence)
         {
-            groupeCompetence.NoProgramme = this.HttpContext.Session.GetString("programme");
-            if (ModelState.IsValid)
+            try
             {
-                _context.Add(groupeCompetence);
-                await _context.SaveChangesAsync();
-                return Ok("ajout reussi");
+                groupeCompetence.NoProgramme = this.HttpContext.Session.GetString("programme");
+                if (ModelState.IsValid)
+                {
+                    _context.Add(groupeCompetence);
+                    await _context.SaveChangesAsync();
+                    return Ok("ajout reussi");
+                }
+
+                return BadRequest("groupe non ajouté");
             }
-           
-            return BadRequest("groupe non ajouté");
+            catch (Exception e)
+            {
+                return View("\\Views\\Shared\\page_erreur.cshtml");
+            }
+            
         }
 
         // GET: GroupeCompetences/Edit/5
         public async Task<IActionResult> Edit(string id)
         {
-            if (id == null)
+            try
             {
-                return NotFound();
-            }
+                if (id == null)
+                {
+                    return NotFound();
+                }
 
-            var groupeCompetence = await _context.GroupeCompetence.FindAsync(id);
-            if (groupeCompetence == null)
-            {
-                return NotFound();
+                var groupeCompetence = await _context.GroupeCompetence.FindAsync(id);
+                if (groupeCompetence == null)
+                {
+                    return NotFound();
+                }
+                ViewData["CodeCompetence"] = new SelectList(_context.Competences, "CodeCompetence", "CodeCompetence", groupeCompetence.CodeCompetence);
+                ViewData["NomGroupe"] = new SelectList(_context.Groupe, "NomGroupe", "NomGroupe", groupeCompetence.NomGroupe);
+                return View(groupeCompetence);
             }
-            ViewData["CodeCompetence"] = new SelectList(_context.Competences, "CodeCompetence", "CodeCompetence", groupeCompetence.CodeCompetence);
-            ViewData["NomGroupe"] = new SelectList(_context.Groupe, "NomGroupe", "NomGroupe", groupeCompetence.NomGroupe);
-            return View(groupeCompetence);
+            catch (Exception e)
+            {
+                return View("\\Views\\Shared\\page_erreur.cshtml");
+            }
+            
         }
 
         // POST: GroupeCompetences/Edit/5
@@ -99,54 +139,71 @@ namespace projetfinalFJO.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(string id, [Bind("NomGroupe,CodeCompetence,NoProgramme")] GroupeCompetence groupeCompetence)
         {
-            if (id != groupeCompetence.NomGroupe)
+            try
             {
-                return NotFound();
-            }
+                if (id != groupeCompetence.NomGroupe)
+                {
+                    return NotFound();
+                }
 
-            if (ModelState.IsValid)
-            {
-                try
+                if (ModelState.IsValid)
                 {
-                    _context.Update(groupeCompetence);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!GroupeCompetenceExists(groupeCompetence.NomGroupe))
+                    try
                     {
-                        return NotFound();
+                        _context.Update(groupeCompetence);
+                        await _context.SaveChangesAsync();
                     }
-                    else
+                    catch (DbUpdateConcurrencyException)
                     {
-                        throw;
+                        if (!GroupeCompetenceExists(groupeCompetence.NomGroupe))
+                        {
+                            return NotFound();
+                        }
+                        else
+                        {
+                            throw;
+                        }
                     }
+                    return RedirectToAction(nameof(Index));
                 }
-                return RedirectToAction(nameof(Index));
+                ViewData["CodeCompetence"] = new SelectList(_context.Competences, "CodeCompetence", "CodeCompetence", groupeCompetence.CodeCompetence);
+                ViewData["NomGroupe"] = new SelectList(_context.Groupe, "NomGroupe", "NomGroupe", groupeCompetence.NomGroupe);
+                return View(groupeCompetence);
             }
-            ViewData["CodeCompetence"] = new SelectList(_context.Competences, "CodeCompetence", "CodeCompetence", groupeCompetence.CodeCompetence);
-            ViewData["NomGroupe"] = new SelectList(_context.Groupe, "NomGroupe", "NomGroupe", groupeCompetence.NomGroupe);
-            return View(groupeCompetence);
+            catch (Exception e)
+            {
+                return View("\\Views\\Shared\\page_erreur.cshtml");
+            }
+            
+            
         }
 
         // GET: GroupeCompetences/Delete/5
         public async Task<IActionResult> Delete(string id)
         {
-            if (id == null)
+            try
             {
-                return NotFound();
-            }
+                if (id == null)
+                {
+                    return NotFound();
+                }
 
-            var groupeCompetence = await _context.GroupeCompetence
-                .Include(g => g.CodeCompetenceNavigation)
-                .Include(g => g.NomGroupeNavigation)
-                .FirstOrDefaultAsync(m => m.NomGroupe == id);
-            if (groupeCompetence == null)
+                var groupeCompetence = await _context.GroupeCompetence
+                    .Include(g => g.CodeCompetenceNavigation)
+                    .Include(g => g.NomGroupeNavigation)
+                    .FirstOrDefaultAsync(m => m.NomGroupe == id);
+                if (groupeCompetence == null)
+                {
+                    return NotFound();
+                }
+
+                return View(groupeCompetence);
+            }
+            catch (Exception e)
             {
-                return NotFound();
+                return View("\\Views\\Shared\\page_erreur.cshtml");
             }
-
-            return View(groupeCompetence);
+            
         }
 
         // POST: GroupeCompetences/Delete/5
@@ -154,10 +211,18 @@ namespace projetfinalFJO.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(string id)
         {
-            var groupeCompetence = await _context.GroupeCompetence.FindAsync(id);
-            _context.GroupeCompetence.Remove(groupeCompetence);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            try
+            {
+                var groupeCompetence = await _context.GroupeCompetence.FindAsync(id);
+                _context.GroupeCompetence.Remove(groupeCompetence);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception e)
+            {
+                return View("\\Views\\Shared\\page_erreur.cshtml");
+            }
+            
         }
 
         private bool GroupeCompetenceExists(string id)
