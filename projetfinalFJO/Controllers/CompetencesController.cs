ca37d73ca911907fae467a265c992895c5ad591d
Joel Stephane Loic Ngando-Kouo <1591982@cegepoutaouais.qc.ca>
2018-12-18 17:35:42 -0500

    compétences try catch

--- projetfinalFJO/Controllers/CompetencesController.cs
+++ projetfinalFJO/Controllers/CompetencesController.cs
@@ -23,32 +23,56 @@ namespace projetfinalFJO.Controllers
         }
 
         // GET: Competences
-        public async Task<IActionResult> ListeCompetence(string search)
+        public async Task<IActionResult> ListeCompetence()
         {
-            return View(await _context.Competences.Where(x => x.Titre.StartsWith(search) && x.NoProgramme == this.HttpContext.Session.GetString("programme") || x.CodeCompetence.StartsWith(search) && x.NoProgramme == this.HttpContext.Session.GetString("programme") || search == null && x.NoProgramme == this.HttpContext.Session.GetString("programme")).ToListAsync());
+            try
+            {
+                return View(await _context.Competences.Where(x => x.NoProgramme == this.HttpContext.Session.GetString("programme")).ToListAsync());
+            }
+            catch (Exception e)
+            {
+                return View("\\Views\\Shared\\page_erreur.cshtml");
+            }
+           
         }
 
         // GET: Competences/Details/5
         public async Task<IActionResult> Details(string id)
         {
-            if (id == null)
+            try
             {
-                return NotFound();
-            }
+                if (id == null)
+                {
+                    return NotFound();
+                }
 
-            ViewData["competence"]= await _context.Competences
-                .Include(c => c.NomFamilleNavigation)
-                .Include(c => c.NoProgrammeNavigation)
-                .FirstOrDefaultAsync(m => m.CodeCompetence == id);
-            ViewData["listelement"] =  _context.CompetencesElementCompetence.ToList().FindAll(x => x.CodeCompetence == id);           
-            return View();
+                ViewData["competence"] = await _context.Competences
+                    .Include(c => c.NomFamilleNavigation)
+                    .Include(c => c.NoProgrammeNavigation)
+                    .FirstOrDefaultAsync(m => m.CodeCompetence == id);
+                ViewData["listelement"] = _context.CompetencesElementCompetence.ToList().FindAll(x => x.CodeCompetence == id);
+                return View();
+            }
+            catch (Exception e)
+            {
+                return View("\\Views\\Shared\\page_erreur.cshtml");
+            }
+            
         }
 
         // GET: Competences/Create
         public IActionResult Create()
         {
-            ViewBag.competence = new Competences();
-            return View();
+            try
+            {
+                ViewBag.competence = new Competences();
+                return View();
+            }
+            catch (Exception e)
+            {
+                return View("\\Views\\Shared\\page_erreur.cshtml");
+            }
+            
         }
 
         // POST: Competences/Create
@@ -58,52 +82,76 @@ namespace projetfinalFJO.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([FromBody][Bind("CodeCompetence,ObligatoireCégep,Titre,Description,ContextRealisation")] Competences competences)
         {
-            competences.NoProgramme = this.HttpContext.Session.GetString("programme");
-            if (ModelState.IsValid)
+            try
             {
-                //Mettre la session a cette compétence
-                this.HttpContext.Session.SetString("Competence", JsonConvert.SerializeObject(competences));
-                //Ajouter a la bd
-                _context.Add(competences);
-                await _context.SaveChangesAsync();
-                return Ok("élément ajouté avec succès");
+                competences.NoProgramme = this.HttpContext.Session.GetString("programme");
+                if (ModelState.IsValid)
+                {
+                    //Mettre la session a cette compétence
+                    this.HttpContext.Session.SetString("Competence", JsonConvert.SerializeObject(competences));
+                    //Ajouter a la bd
+                    _context.Add(competences);
+                    await _context.SaveChangesAsync();
+                    return Ok("élément ajouté avec succès");
+                }
+                ViewData["Idfamille"] = new SelectList(_context.Famillecompetence, "Idfamille", "NomFamille", competences.NomFamille);
+                ViewData["NoProgramme"] = new SelectList(_context.Programmes, "NoProgramme", "NoProgramme", competences.NoProgramme);
+                return BadRequest("élément non ajouté");
             }
-            ViewData["Idfamille"] = new SelectList(_context.Famillecompetence, "Idfamille", "NomFamille", competences.NomFamille);
-            ViewData["NoProgramme"] = new SelectList(_context.Programmes, "NoProgramme", "NoProgramme", competences.NoProgramme);
-            return BadRequest("élément non ajouté");
+            catch (Exception e)
+            {
+                return View("\\Views\\Shared\\page_erreur.cshtml");
+            }
+            
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Update([FromBody][Bind("CodeCompetence,ObligatoireCégep,Titre,Description,ContextRealisation")] Competences competences)
         {
-            competences.NoProgramme = this.HttpContext.Session.GetString("programme");
-            if (ModelState.IsValid)
+            try
             {
-                _context.Update(competences);
-                // https://stackoverflow.com/questions/25894587/how-to-update-record-using-entity-framework-6
-                await _context.SaveChangesAsync();
-                return Ok("élément ajouté avec succès");
+                competences.NoProgramme = this.HttpContext.Session.GetString("programme");
+                if (ModelState.IsValid)
+                {
+                    _context.Update(competences);
+                    // https://stackoverflow.com/questions/25894587/how-to-update-record-using-entity-framework-6
+                    await _context.SaveChangesAsync();
+                    return Ok("élément ajouté avec succès");
+                }
+                ViewData["Idfamille"] = new SelectList(_context.Famillecompetence, "Idfamille", "NomFamille", competences.NomFamille);
+                ViewData["NoProgramme"] = new SelectList(_context.Programmes, "NoProgramme", "NoProgramme", competences.NoProgramme);
+                return BadRequest("élément non ajouté");
             }
-            ViewData["Idfamille"] = new SelectList(_context.Famillecompetence, "Idfamille", "NomFamille", competences.NomFamille);
-            ViewData["NoProgramme"] = new SelectList(_context.Programmes, "NoProgramme", "NoProgramme", competences.NoProgramme);
-            return BadRequest("élément non ajouté");
+            catch (Exception e)
+            {
+                return View("\\Views\\Shared\\page_erreur.cshtml");
+            }
+           
         }
 
         // GET: Competences/Edit/5
         public async Task<IActionResult> ModifierCompetence(string id)
         {
-            if (id == null)
+            try
             {
-                return NotFound();
-            }
+                if (id == null)
+                {
+                    return NotFound();
+                }
 
-            var competences = await _context.Competences.FindAsync(id);
-            if (competences == null)
+                var competences = await _context.Competences.FindAsync(id);
+                if (competences == null)
+                {
+                    return NotFound();
+                }
+                return View(competences);
+            }
+            catch (Exception e)
             {
-                return NotFound();
-            }            
-            return View(competences);
+                return View("\\Views\\Shared\\page_erreur.cshtml");
+            }
+            
         }
 
         // POST: Competences/Edit/5
@@ -113,6 +161,34 @@ namespace projetfinalFJO.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> ModifierCompetence(string id, [Bind("CodeCompetence,ObligatoireCégep,Titre,Description,ContextRealisation,Idfamille,NoProgramme")] Competences competences)
         {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    try
+                    {
+                        _context.Update(competences);
+                        await _context.SaveChangesAsync();
+                    }
+                    catch (DbUpdateConcurrencyException)
+                    {
+                        if (!CompetencesExists(competences.CodeCompetence))
+                        {
+                            return NotFound();
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                    return RedirectToAction(nameof(ListeCompetence));
+                }
+                return View(competences);
+            }
+            catch (Exception e)
+            {
+                return View("\\Views\\Shared\\page_erreur.cshtml");
+            }
             //Prend rel enuméro du programme
             competences.NoProgramme = this.HttpContext.Session.GetString("programme");
 
@@ -121,47 +197,35 @@ namespace projetfinalFJO.Controllers
                 return NotFound();
             }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(competences);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CompetencesExists(competences.CodeCompetence))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(ListeCompetence));
-            }            
-            return View(competences);
+            
         }
 
         // GET: Competences/Delete/5
         public async Task<IActionResult> Supprimer_Compétence(string id)
         {
-            if (id == null)
+            try
             {
-                return NotFound();
-            }
+                if (id == null)
+                {
+                    return NotFound();
+                }
 
-            var competences = await _context.Competences
-                .Include(c => c.NomFamilleNavigation)
-                .Include(c => c.NoProgrammeNavigation)
-                .FirstOrDefaultAsync(m => m.CodeCompetence == id);
-            if (competences == null)
+                var competences = await _context.Competences
+                    .Include(c => c.NomFamilleNavigation)
+                    .Include(c => c.NoProgrammeNavigation)
+                    .FirstOrDefaultAsync(m => m.CodeCompetence == id);
+                if (competences == null)
+                {
+                    return NotFound();
+                }
+
+                return View(competences);
+            }
+            catch (Exception e)
             {
-                return NotFound();
+                return View("\\Views\\Shared\\page_erreur.cshtml");
             }
-
-            return View(competences);
+            
         }
 
         // POST: Competences/Delete/5
@@ -169,10 +233,18 @@ namespace projetfinalFJO.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(string id)
         {
-            var competences = await _context.Competences.FindAsync(id);
-            _context.Competences.Remove(competences);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(ListeCompetence));
+            try
+            {
+                var competences = await _context.Competences.FindAsync(id);
+                _context.Competences.Remove(competences);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(ListeCompetence));
+            }
+            catch (Exception e)
+            {
+                return View("\\Views\\Shared\\page_erreur.cshtml");
+            }
+            
         }
 
         private bool CompetencesExists(string id)
@@ -182,16 +254,31 @@ namespace projetfinalFJO.Controllers
 
         public PartialViewResult partialElementcompetence()
         {
-            ViewBag.Elementcompetence = new Elementcompetence();
-            return PartialView("_PartialElementcompetence");
+            try
+            {
+                ViewBag.Elementcompetence = new Elementcompetence();
+                return PartialView("_PartialElementcompetence");
+            }
+            catch (Exception e)
+            {
+                return PartialView("\\Views\\Shared\\page_erreur.cshtml");
+            }
+            
         }
         public PartialViewResult partialCompElement()
         {
-
-            ViewBag.SessionRep = new CompetencesElementCompetence();
-            ViewData["CodeCompetence"] = new SelectList(_context.Competences.Where(x => x.NoProgramme.Equals(this.HttpContext.Session.GetString("programme"))), "CodeCompetence", "CodeCompetence");
-            ViewData["ElementCompétence"] = new SelectList(_context.Elementcompetence.Where(x => x.NoProgramme.Equals(this.HttpContext.Session.GetString("programme"))), "ElementCompétence", "ElementCompétence");
-            return PartialView("_PartialCompElement");
+            try
+            {
+                ViewBag.SessionRep = new CompetencesElementCompetence();
+                ViewData["CodeCompetence"] = new SelectList(_context.Competences.Where(x => x.NoProgramme.Equals(this.HttpContext.Session.GetString("programme"))), "CodeCompetence", "CodeCompetence");
+                ViewData["ElementCompétence"] = new SelectList(_context.Elementcompetence.Where(x => x.NoProgramme.Equals(this.HttpContext.Session.GetString("programme"))), "ElementCompétence", "ElementCompétence");
+                return PartialView("_PartialCompElement");
+            }
+            catch (Exception e)
+            {
+                return PartialView("\\Views\\Shared\\page_erreur.cshtml");
+            }
+            
         }
     }
 }
